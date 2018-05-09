bc4d760274c64ec3dee4eaf9dad23cee811c5176
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2018-05-09 12:20:33 +0000

    Create Header component

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+// import libraries to help create a component
+import React from 'react';
 import { AppRegistry } from 'react-native';
-import App from './App';
+import Header from './src/components/Header';
 
+// create component
+const App = () => (
+  <Header headerText='Albums' />
+);
+
+// render the component
 AppRegistry.registerComponent('albums', () => App);
